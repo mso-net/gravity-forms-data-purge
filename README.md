4fb8a62ec58ba6bcddcf632ea96ed16f243a26f4
Andrew Dixon <andrew.dixon@mso.net>
2018-04-04 16:47:18 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# gravity-forms-data-purge
+
+A WordPress plugin to purge Gravity Forms Entries that are old that a set number of day to help with GDPR compliance.
+
+The plugin is very simple. It adds a setting to the Gravity Forms menu in the WP Admin called “Purge Data” and under this option you set the number of days you would like to retain Gravity Forms entries for, for all forms on your site. 
+
+The default is to keep all entries forever, empty field, and a valid value is any number of days from 0 (zero) to X. Once an entry is that number of days old, it is removed. 
+
+The cron task to delete the entries runs once per hour, so when set to 0, the maximum time an entry will be stored for is one hour. 
+
+**PLEASE NOTE:** Entries are permanently deleted, so use with care.
---
+++ gravity-forms-data-purge.php
@@ -0,0 +1,80 @@
+<?php
+/*
+Plugin Name: Gravity Forms Data Purge
+Plugin URI: https://github.com/mso-net/gravity-forms-data-purge/
+Description: Adds the ability to purge gravity form entries after a certain amount of time to comply with GDPR.
+Author: mso.net
+Version: 1.0.1
+Author URI: https://www.mso.net/
+Text Domain: gravity-forms-data-purge
+Domain Path: /languages
+License: GPLv2
+*/
+
+function gfdp_register_settings() {
+	add_option( 'gfdp_option_name', '');
+   	register_setting( 'gfdp_options_group', 'gfdp_option_name', 'gfdp_callback' );
+}
+add_action( 'admin_init', 'gfdp_register_settings' );
+
+
+add_filter( 'gform_addon_navigation', 'create_menu' );
+function create_menu( $menus ) {
+	$menus[] = array( 'name' => 'Purge Data', 'label' => __( 'Purge Data' ), 'callback' =>  'gfdp_options_page' );
+  	return $menus;
+}
+
+function gfdp_options_page(){
+	echo '<div>';
+		echo '<h2>Gravity Forms Data Purge Settings</h2>';
+		echo '<form method="post" action="options.php">';
+			settings_fields( 'gfdp_options_group' );
+			echo "<h3>How long should the system keep your gravity form entries?</h3>";
+			echo "<p>Please provide the number of days that you would like to keep gravity form entries on the server.</p>";
+			echo '<table>';
+				echo '<tr valign="top">';
+					echo '<th scope="row"><label for="gfdp_option_name">Number of days (leave empty to keep data indefinitely)</label></th>';
+					echo '<td><input type="text" id="gfdp_option_name" name="gfdp_option_name" value="'.get_option('gfdp_option_name').'" /></td>';
+				echo '</tr>';
+			echo '</table>';
+			submit_button();
+		echo '</form>';
+	echo '</div>';
+}
+
+
+add_action('check_for_expired_entries', 'checkForExpiredEntries');
+
+function activateCheckForExpiredEntries() {
+	if ( !wp_next_scheduled( 'check_for_expired_entries' ) ) {
+		wp_schedule_event( current_time( 'timestamp' ), 'hourly', 'check_for_expired_entries');
+	}
+}
+
+add_action('wp', 'activateCheckForExpiredEntries');
+
+
+function checkForExpiredEntries() {
+	if( get_option('gfdp_option_name') != '' ){
+
+        //see which entries are over the allowed time
+		$search_criteria = array();
+		$form_id = 1;
+
+		$expiredEntryTime = '-'.get_option('gfdp_option_name').' days';
+
+		$start_date = date( 'Y-m-d', strtotime('-365 days') );
+		$end_date = date( 'Y-m-d', strtotime( $expiredEntryTime ));
+		$paging = array( 'offset' => 0, 'page_size' => 10000 );
+		$search_criteria['start_date'] = $start_date;
+		$search_criteria['end_date'] = $end_date;
+		 
+		$entries = GFAPI::get_entries(0, $search_criteria, null, $paging);
+
+		foreach ($entries as $entry) {
+			//delete the entry from the system
+			$deleteit = GFAPI::delete_entry($entry);
+			$deleteitfromtrash = GFAPI::delete_entry($entry['id']);	
+		}
+	}        
+}
